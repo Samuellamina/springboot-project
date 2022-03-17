1af35565ff7f7e9f0ad51ea1de34277519ae9f29
Lamina Samuel <samuellamina2@gmail.com>
2022-03-17 14:24:34 +0100

    rename files

--- src/main/java/io/sam/project/securityAlt/services/UserDetailsImplAlt.java
+++ src/main/java/io/sam/project/securityAlt/services/UserDetailsImplAlt.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-public class UserDetailsImplTwo implements UserDetails {
+public class UserDetailsImplAlt implements UserDetails {
 
     private static final long serialVersionUID = 1L;
     private Long id;
@@ -22,19 +22,19 @@ public class UserDetailsImplTwo implements UserDetails {
 
     private Collection<? extends GrantedAuthority> authorities;
 
-    public UserDetailsImplTwo(Long id, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+    public UserDetailsImplAlt(Long id, String username, String password, Collection<? extends GrantedAuthority> authorities) {
         this.id = id;
         this.username = username;
         this.password = password;
         this.authorities = authorities;
     }
 
-    public static UserDetailsImplTwo build(User user) {
+    public static UserDetailsImplAlt build(User user) {
         List<GrantedAuthority> authorities = user.getRoles().stream()
                 .map(role -> new SimpleGrantedAuthority(role.getName().name()))
                 .collect(Collectors.toList());
 
-        return new UserDetailsImplTwo(
+        return new UserDetailsImplAlt(
                 user.getId(),
                 user.getUsername(),
                 user.getPassword(),
@@ -87,7 +87,7 @@ public class UserDetailsImplTwo implements UserDetails {
             return true;
         if (o == null || getClass() != o.getClass())
             return false;
-        UserDetailsImplTwo user = (UserDetailsImplTwo) o;
+        UserDetailsImplAlt user = (UserDetailsImplAlt) o;
         return Objects.equals(id, user.id);
     }
 }

a77f28fc70e4c433bc2c44033c55e71e4142902a
Lamina Samuel <samuellamina2@gmail.com>
2022-03-17 14:28:15 +0100

    refactors

--- src/main/java/io/sam/project/controllers/AlternateAuthController.java
+++ src/main/java/io/sam/project/controllers/AlternateAuthController.java
@@ -3,6 +3,7 @@ package io.sam.project.controllers;
 import io.sam.project.payload.request.LoginRequest;
 import io.sam.project.payload.request.SignupRequest;
 import io.sam.project.securityAlt.services.UserServiceAlt;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,16 +15,15 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth2")
+@RequiredArgsConstructor
 public class AlternateAuthController {
-    @Autowired
-    private UserServiceAlt userService;
+
+    private final UserServiceAlt userService;
 
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
         return userService.signin(loginRequest);
     }
-
-    // https://github.com/koushikkothagal/spring-security-jwt
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
